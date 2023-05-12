b61d5428cb14e923cfd412fa55cb7ffab08407cc
Jzridar <jzridar@gmail.com>
2023-05-11 21:00:21 -0400

    files added

---
+++ server.js
@@ -0,0 +1,29 @@
+// Load express
+const express = require('express');
+const pokemon = require('./models/pokemon.js');
+const port = 3000
+// Create our express app
+const app = express();
+
+app.set("view engine","jsx");
+app.engine("jsx",require("jsx-view-engine").createEngine())
+
+// Define a "root" route directly on app
+// Tomorrow, we'll use best practice routing
+app.get('/', function (req, res) {
+  res.send('<h1>Welcome to the Pokemon App!</h1>');
+});
+
+
+app.get('/pokemon/', (req, res)=> {
+    res.render('Index', { pokemon: pokemon });
+  });
+
+  app.get('/pokemon/:id',(req,res)=>{
+    res.render('show',{pokemon:pokemon[req.params.id]})
+  })
+// Tell the app to listen on port 3000
+// for HTTP requests from clients
+app.listen(port, function () {
+  console.log('Listening on port 3000');
+});
---
+++ views/Index.jsx
@@ -0,0 +1,39 @@
+
+
+
+
+const React = require('react');
+
+const myStyle ={
+  color : 'gray',
+  backgroundColor: 'ligitblue',
+
+};
+
+class Index extends React.Component {
+  render() {
+      const { pokemon } = this.props;
+      return (
+              <div style = {myStyle}>
+                  <h1>See All the pokemon</h1>
+                  <ul>
+                      {pokemon .map((pokemon, i) => {
+                        return (
+                          <li>
+                            The {' '}
+                        <a href={`/pokemon /${i}`}> {pokemon.name.toUpperCase()} 
+                        </a> {' '}
+                                      
+                          is <br /> <img src= {pokemon.img + ".jpg" }/> <br></br>
+                              </li>
+                          );
+                      })}
+                  </ul>
+                  <nav>
+                    <a href="/pokemon/new">Create a New pokemon</a>
+                </nav>
+              </div>
+      );
+  }
+}
+module.exports = Index;
